eab0bc272235f128f1d0b137a11d0d1a740e8dc1
Jeongbin Park <pjb7687@snu.ac.kr>
2015-01-29 01:17:49 +0900

    Patch to support non-human organisms

---
+++ build_find_position_bam.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+g++ -O3 1.find_position_bam.cpp -o 1.find_position_bam -I ~/pjb/20141013_Digenome_VEGFA_1_2_3/digenome-toolkit/bamtools/include -L ~/pjb/20141013_Digenome_VEGFA_1_2_3/digenome-toolkit/bamtools/lib -lbamtools -lz
