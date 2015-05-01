767db5e733ccf657d420ad6ca672ce68c1cba12b
Jeongbin Park <pjb7687@gmail.com>
2015-05-01 20:04:32 +0900

    Added sum_range argument

--- 3.count.py
+++ 3.count.py
@@ -1,20 +1,40 @@
 from sys import argv
+from collections import defaultdict
 
-for fn in argv[1:]:
+range_sum = 0
+fns = []
+for i in range(1, len(argv)):
+    if i != len(argv)-1 and argv[i] == "-u":
+        range_sum = int(argv[i+1])
+    elif i != 0 and argv[i-1] != "-u":
+        fns.append(argv[i])
+
+bufs = defaultdict(lambda: 0)
+for fn in fns:
     print ('Processing {}...'.format(fn))
     fnhead = '.'.join(fn.split('.')[:-1])
     f = open(fn)
-    fo = open(fnhead + '_freq.txt', 'w')
     prev = ""
     cnt = 0
     for line in f:
         line = line.strip()
         if line != prev:
             if cnt != 0:
-                fo.write("%s\t%d\n"%(prev, cnt))
+                pos = int(prev)
+                for i in range(max(0, pos-range_sum), pos+range_sum+1):
+                    bufs[i] += cnt
             prev = line
             cnt = 0
         cnt += 1
-    fo.write("%s\t%d"%(prev, cnt))
-    fo.close()
     f.close()
+
+    pos = int(prev)
+    for i in range(max(0, pos-range_sum), pos+range_sum+1):
+        bufs[i] += cnt
+
+    l = list(bufs.items())
+    l.sort(key=lambda e:e[0])
+    with open(fnhead + '_freq.txt', 'w') as fo:
+        for key, val in l:
+            fo.write("%d\t%d\n"%(key, val))
+    
